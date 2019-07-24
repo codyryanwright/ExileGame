ac1002529a07b7227d4c6010a3e931d54cabbf32
Lucian Turk <lucianturk21@gmail.com>
2019-07-24 14:11:26 -0500

    Updated compareTest with more test cases

--- Game/src/tests/GameControllerTest.java
+++ Game/src/tests/GameControllerTest.java
@@ -19,8 +19,10 @@ public class GameControllerTest {
 		Card c1 = new Card("red", "dragon", "rare", 4.0f, "");
 		Card c2 = new Card("blue", "dragon", "rare", 4.0f, "");
 		Card c3 = new Card("blue", "knight", "rare", 4.0f, "");
+		Card c4 = new Card("green", "knight", "rare", 4.0f, "");
+		Card c5 = new Card("green", "wizard", "rare", 4.0f, "");
 		//Same type, different archetype
-		gc.compareCards(c2, c3);
+		gc.compareCards(c2, c4);
 		assertEquals(gc.user.getHealth(), 80f);
 		assertEquals(gc.opponent.getHealth(), 100f);
 		
@@ -33,6 +35,38 @@ public class GameControllerTest {
 		gc.compareCards(c1, c1);
 		assertEquals(gc.user.getHealth(), 80f);
 		assertEquals(gc.opponent.getHealth(), 80f);
+		
+		gc.compareCards(c2, c3);
+		assertEquals(gc.user.getHealth(), 60f);
+		assertEquals(gc.opponent.getHealth(), 80f);
+		
+		//Same type, different archetype
+		gc.compareCards(c4, c2);
+		assertEquals(gc.user.getHealth(), 60f);
+		assertEquals(gc.opponent.getHealth(), 60f);
+		
+		//Different type, same archetype
+		gc.compareCards(c1, c2);
+		assertEquals(gc.user.getHealth(), 40f);
+		assertEquals(gc.opponent.getHealth(), 60f);
+				
+		gc.compareCards(c3, c2);
+		assertEquals(gc.user.getHealth(), 40f);
+		assertEquals(gc.opponent.getHealth(), 40f);
+		
+		gc.compareCards(c1, c5);
+		assertEquals(gc.user.getHealth(), 40f);
+		assertEquals(gc.opponent.getHealth(), 20f);
+		
+		gc.compareCards(c5, c3);
+		assertEquals(gc.user.getHealth(), 40f);
+		assertEquals(gc.opponent.getHealth(), 0f);
+		
+		gc.compareCards(c5, c1);
+		assertEquals(gc.user.getHealth(), 20f);
+		assertEquals(gc.opponent.getHealth(), 0f);
+		
+		
 	}
 		
 		
