4330cbd59dcef1e53d6c66e99ffac4dfd39a76ba
codyryanwright <cwright68@gmail.com>
2019-07-29 20:29:42 -0500

    Fixed deck bug (wasn't pushing to discard), reset game vars upon NewGame

--- Game/src/game/Participant.java
+++ Game/src/game/Participant.java
@@ -17,30 +17,39 @@ public abstract class Participant {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public Deck getDeck() {
 		return deck;
 	}
+	
 	public void setDeck(Deck deck) {
 		this.deck = deck;
 	}
+	
 	public Card[] getHand() {
 		return hand;
 	}
+	
 	public void setHand(Card[] hand) {
 		this.hand = hand;
 	}
+	
 	public int getHealth() {
 		return health;
 	}
+	
 	public void setHealth(int health) {
 		this.health = health;
 	}
+	
 	public void draw() {
 		hand[cardPosition] = deck.getCard();
 	}
+	
 	public void loadHand() {
 		for(int i = 0; i < HAND_SIZE; i++) {
 			cardPosition = i;
@@ -48,17 +57,30 @@ public abstract class Participant {
 		}
 		cardPosition = -1;
 	}
+	
 	public void decreaseHealth() {
 		this.health -= 20;
 	}
+	
 	public int getCardPosition() {
 		return cardPosition;
 	}
+	
 	public void setCardPosition(int cardPosition) {
 		this.cardPosition = cardPosition;
 	}
 
-	public void pushToDiscard(Card userCard) {
-		deck.pushToDiscard(userCard);
+//	public void pushToDiscard(Card userCard) {
+//		deck.pushToDiscard(userCard);
+//	}
+	
+	public void reset() {
+		this.health = 100;
+		cardPosition = -1;
+		if(deck != null) deck.reset(); // reset the current deck (move discard back into deck)
+		deck = null;
+		for(int i = 0; i < HAND_SIZE; i++) {
+			hand[i] = null;
+		}
 	}
 }
