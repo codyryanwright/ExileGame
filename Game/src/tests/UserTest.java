6198a4473cd55e3e6093f9e3529ab702fc3d3f7f
Lucian Turk <lucianturk21@gmail.com>
2019-07-31 17:22:32 -0500

    Added UserTest class and loadHand test

---
+++ Game/src/tests/UserTest.java
@@ -0,0 +1,20 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import game.User;
+import game.Deck;
+
+class UserTest {
+
+	User p1 = new User();
+	Deck d1 = new Deck(1);
+	
+	@Test
+	void loadHandTest() {
+		p1.setDeck(d1);
+		p1.loadHand();
+		assertNotNull(p1.getHand());
+	}
+
+}
