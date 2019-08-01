aca52e0512f3db53172ee4b0f1d88532fbece532
codyryanwright <cwright68@gmail.com>
2019-08-01 11:06:23 -0500

    Added deck tests for Deck(0), Deck(1), and Deck(2)

--- Game/src/tests/DeckTest.java
+++ Game/src/tests/DeckTest.java
@@ -8,9 +8,20 @@ import game.Deck;
 class DeckTest {
 
 	@Test
-	void testDeck() {
+	void testDeck0() {
+		Deck testDeck = new Deck(0);
+		assertEquals(20, testDeck.getDeck().size());
+	}
+	
+	@Test
+	void testDeck1() {
 		Deck testDeck = new Deck(1);
 		assertEquals(20, testDeck.getDeck().size());
 	}
 
+	@Test
+	void testDeck2() {
+		Deck testDeck = new Deck(2);
+		assertEquals(20, testDeck.getDeck().size());
+	}
 }
