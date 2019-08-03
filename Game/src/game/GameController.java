18201d0dc66937f7e3231979cb05356a16751a1a
ArmandoOnTheGo <armando.mendoza1129@gmail.com>
2019-08-03 17:27:32 -0500

    fixed checkWinner

--- Game/src/game/GameController.java
+++ Game/src/game/GameController.java
@@ -13,16 +13,16 @@ public class GameController {
 	private Deck[] collection = new Deck[DECK_TOTAL];
 	private int deckChoice;
 	private UI view;
-	private Participant user, opponent;	
+	private Participant user, opponent;
 	private float playerPower = 0, opponentPower = 0;
-	
+
 	public GameController(UI view) {
 		deckChoice = -1;
 		this.view = view;
 		user = new User();
 		opponent = new AutoOpponent();
 		buildCollection();
-	
+
 		view.addNewGameListener(new NewGameListener());
 		view.addDeckListener(new DeckListener());
 		view.addCardListener(new CardListener());
@@ -43,66 +43,64 @@ public class GameController {
 			view.getTxtpnGame().setFont(new Font("SimSun", Font.PLAIN, 9));
 		}
 	}
-	
+
 	class DeckListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == view.getBtnDeck0()) 
+			if (e.getSource() == view.getBtnDeck0())
 				deckChoice = 0;
-			else if(e.getSource() == view.getBtnDeck1())
+			else if (e.getSource() == view.getBtnDeck1())
 				deckChoice = 1;
-			else if(e.getSource() == view.getBtnDeck2())
+			else if (e.getSource() == view.getBtnDeck2())
 				deckChoice = 0;
-			
+
 			user.setDeck(collection[deckChoice]);
 			user.loadHand();
 			updateHand();
-			
-			//sets the opponents deck randomly
+
+			// sets the opponents deck randomly
 			int choice = new Random().nextInt(2);
 			opponent.setDeck(collection[choice]);
 			opponent.loadHand();
 			view.show("playPanel");
 		}
 	}
-	
+
 	class CardListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == view.getBtnCard0())
+			if (e.getSource() == view.getBtnCard0())
 				user.setCardPosition(0);
-			else if(e.getSource() == view.getBtnCard1())
+			else if (e.getSource() == view.getBtnCard1())
 				user.setCardPosition(1);
-			else if(e.getSource() == view.getBtnCard2()) 
+			else if (e.getSource() == view.getBtnCard2())
 				user.setCardPosition(2);
 			view.setChosenBorder(user.getCardPosition());
 		}
 	}
-	
-	
-	class MullListener implements ActionListener{
+
+	class MullListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(user.getCardPosition() == 0 ||
-					user.getCardPosition() == 1 ||
-						user.getCardPosition() == 2) {
+			if (user.getCardPosition() == 0 || user.getCardPosition() == 1 || user.getCardPosition() == 2) {
 				user.mulligan();
 				checkWinner();
+				user.setCardPosition(-1); // resets card position
+				view.setChosenBorder(user.cardPosition);
 				view.setHealth(user.getHealth(), opponent.getHealth());
 				updateHand();
 			}
 		}
 	}
-	
+
 	/**
-	 * Sets the game play in motion by calling Participants' playCard() and passing to combatCards()
+	 * Sets the game play in motion by calling Participants' playCard() and passing
+	 * to combatCards()
 	 */
 	class PlayCardListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			if (user.getCardPosition() == -1) {
 				view.appendText("Please choose a Card!");
 				view.refreshText();
-			}
-			else
-			{
-				//gets a card from the user and the opponent
+			} else {
+				// gets a card from the user and the opponent
 				Card userCard = ((User) user).playCard();
 				Card opCard = ((AutoOpponent) opponent).playCard();
 				combatCards(userCard, opCard);
@@ -110,114 +108,101 @@ public class GameController {
 			}
 		}
 	}
-	
+
 	class ContinueListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(deckChoice == -1)
+			if (deckChoice == -1)
 				view.show("choosePanel");
 			else
 				view.show("playPanel");
 		}
 	}
-	
+
 	class DifficultyListener implements ChangeListener {
 		@Override
 		public void stateChanged(ChangeEvent e) {
 			if (view.getDifficulty() == "Hard")
 				((AutoOpponent) opponent).setDifficulty(0);
-			else ((AutoOpponent) opponent).setDifficulty(1);
+			else
+				((AutoOpponent) opponent).setDifficulty(1);
 		}
 	}
-	
+
 	public void updateHand() {
-		for(int i = 0; i < 3; i++)
+		for (int i = 0; i < 3; i++)
 			view.setImageIcon(i, user.hand[i].getImgIconCard());
 	}
-	
+
 	public void buildCollection() {
 		try {
-			for(int i = 0; i < DECK_TOTAL; i++)
+			for (int i = 0; i < DECK_TOTAL; i++)
 				collection[i] = new Deck(i);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void setDeckChoice(int deckChoice) {
 		this.deckChoice = deckChoice;
 	}
-	
+
 	public int getDeckChoice() {
 		return deckChoice;
 	}
-	
+
 	public Deck getDeck() {
 		return collection[deckChoice];
 	}
-	
+
 	/**
-	 * Handles the output for a given card match-up both pre- and post-call to compareCards, then resets game for next play.
+	 * Handles the output for a given card match-up both pre- and post-call to
+	 * compareCards, then resets game for next play.
 	 * 
-	 * @param userCard  the card that has been chosen by the user
-	 * @param opponentCard  the card that has been chosen by the opponent
+	 * @param userCard     the card that has been chosen by the user
+	 * @param opponentCard the card that has been chosen by the opponent
 	 */
 	public void combatCards(Card userCard, Card opponentCard) {
 		playerPower = 0;
 		opponentPower = 0;
 		// Print cards played
-		view.appendText("You played "+userCard.getName() +
-				"\nYour Opponent played "+opponentCard.getName());
+		view.appendText("You played " + userCard.getName() + "\nYour Opponent played " + opponentCard.getName());
 		view.refreshText();
-		
+
 		int battleResult = compareCards(userCard, opponentCard);
 
 		// Print match results
-		view.appendText("Match Results: \nYour "+userCard.getName()+ 
-				" power: "+playerPower+"\nYour Opponent's "+opponentCard.getName()+
-				" power: "+opponentPower);
-		
+		view.appendText("Match Results: \nYour " + userCard.getName() + " power: " + playerPower + "\nYour Opponent's "
+				+ opponentCard.getName() + " power: " + opponentPower);
+
 		// Print outcome
 		if (battleResult == 0) {
 			user.decreaseHealth();
 			view.appendText("\nYou've lost this round!\n");
 			view.refreshText();
-		}
-		else if (battleResult == 1) {
+		} else if (battleResult == 1) {
 			opponent.decreaseHealth();
 			view.appendText("\nYou've won this round!\n");
 			view.refreshText();
-		}
-		else {
+		} else {
 			view.appendText("\nIt's a draw!\n");
 			view.refreshText();
 		}
-		
-		// Check health status
-		if(user.getHealth() == 0)
-		{
-			view.setEndMessage("YOU LOSE!");
-			view.show("endPanel");
-		}
-		else if (opponent.getHealth() == 0)
-		{
-			view.setEndMessage("YOU WIN!");
-			view.show("endPanel");
-		}
-		else // Update hand for play
-		{
-			view.setHealth(user.getHealth(), opponent.getHealth());
-			user.draw();
-			opponent.draw();
-			updateHand();
-			user.setCardPosition(-1); // resets card position
-		}
+
+		checkWinner();
+		view.setHealth(user.getHealth(), opponent.getHealth());
+		user.draw();
+		opponent.draw();
+		updateHand();
+		user.setCardPosition(-1); // resets card position
+		view.setChosenBorder(user.cardPosition);
 	}
-	
+
 	/**
-	 * Makes the comparison of the card matchups and modifies the card power accordingly to determine a winner.
+	 * Makes the comparison of the card matchups and modifies the card power
+	 * accordingly to determine a winner.
 	 * 
-	 * @param userCard  the card that has been chosen by the user
-	 * @param opponentCard  the card that has been chosen by the opponent
+	 * @param userCard     the card that has been chosen by the user
+	 * @param opponentCard the card that has been chosen by the opponent
 	 * @return returns a switch case for win/lose/draw for the round
 	 */
 	public int compareCards(Card userCard, Card opponentCard) {
@@ -225,7 +210,8 @@ public class GameController {
 		opponentPower = opponentCard.getPower();
 
 		// Make type comparison and modify participants power
-		if (userCard.getType().equals(opponentCard.getType())); // do nothing
+		if (userCard.getType().equals(opponentCard.getType()))
+			; // do nothing
 		else if ((userCard.getType().equals("red") && opponentCard.getType().equals("green"))
 				|| (userCard.getType().equals("green") && opponentCard.getType().equals("blue"))
 				|| (userCard.getType().equals("blue") && opponentCard.getType().equals("red")))
@@ -234,38 +220,32 @@ public class GameController {
 			opponentPower += opponentCard.getPower();
 
 		// Make archetype comparison and modify participants power
-		if (userCard.getArchetype().equals(opponentCard.getArchetype())); // do nothing
+		if (userCard.getArchetype().equals(opponentCard.getArchetype()))
+			; // do nothing
 		else if ((userCard.getArchetype().equals("dragon") && opponentCard.getArchetype().equals("wizard"))
 				|| (userCard.getArchetype().equals("wizard") && opponentCard.getArchetype().equals("knight"))
 				|| (userCard.getArchetype().equals("knight") && opponentCard.getArchetype().equals("dragon")))
 			playerPower += 1.5 * userCard.getPower();
 		else
 			opponentPower += 1.5 * opponentCard.getPower();
-		
-		if (opponentPower > playerPower) 
+
+		if (opponentPower > playerPower)
 			return 0;
-		else if (opponentPower < playerPower) 
+		else if (opponentPower < playerPower)
 			return 1;
-		else 
+		else
 			return 2;
 	}
-	
+
 	public void checkWinner() {
-		//check for winner
-		if(user.getHealth() == 0)
-		{
+		// check for winner
+		if (user.getHealth() == 0) {
 			view.setEndMessage("YOU LOSE!");
 			view.show("endPanel");
-		}
-		else if (opponent.getHealth() == 0)
-		{
+		} else if (opponent.getHealth() == 0) {
 			view.setEndMessage("YOU WIN!");
 			view.show("endPanel");
-		}
-		else
-		{
-			user.setCardPosition(-1); // resets card position
-			view.setChosenBorder(user.cardPosition);
-		}
+		} else
+			return;
 	}
 }
